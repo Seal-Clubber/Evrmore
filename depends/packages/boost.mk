85e316a7fdb95d7f920df9c2e178570b28fd3b6a
Seal <seal@example.com>
2025-04-16 22:58:35 +0200

    final

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,6 +1,5 @@
 package=boost
 $(package)_version=1_81_0
-#$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
 $(package)_download_path=https://archives.boost.io/release/1.81.0/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
 $(package)_sha256_hash=71feeed900fbccca04a3b4f2f84a7c217186f28a940ed8b7ed4725986baf99fa
