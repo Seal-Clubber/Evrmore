88cd8249819b42825e9e5390e184be366cf45b34
Seal <seal@example.com>
2025-04-16 20:28:01 +0200

    gcc-11?

--- .github/scripts/00-install-deps.sh
+++ .github/scripts/00-install-deps.sh
@@ -83,10 +83,10 @@ elif [[ ${OS} == "linux" || ${OS} == "linux-disable-wallet" || ${OS} == "aarch64
     ca-certificates \
     curl \
     g++-aarch64-linux-gnu \
-    g++-9-aarch64-linux-gnu \
-    g++-9-multilib \
-    gcc-9-aarch64-linux-gnu \
-    gcc-9-multilib \
+    g++-11-aarch64-linux-gnu \
+    g++-11-multilib \
+    gcc-11-aarch64-linux-gnu \
+    gcc-11-multilib \
     git \
     gnupg \
     libtool \
@@ -111,13 +111,13 @@ elif [[ ${OS} == "arm32v7" || ${OS} == "arm32v7-disable-wallet" ]]; then
     ca-certificates \
     curl \
     g++-aarch64-linux-gnu \
-    g++-9-aarch64-linux-gnu \
-    gcc-9-aarch64-linux-gnu \
+    g++-11-aarch64-linux-gnu \
+    gcc-11-aarch64-linux-gnu \
     g++-arm-linux-gnueabihf \
-    g++-9-arm-linux-gnueabihf \
-    gcc-9-arm-linux-gnueabihf \
-    g++-9-multilib \
-    gcc-9-multilib \
+    g++-11-arm-linux-gnueabihf \
+    gcc-11-arm-linux-gnueabihf \
+    g++-11-multilib \
+    gcc-11-multilib \
     git \
     libtool \
     pkg-config \
@@ -129,6 +129,4 @@ else
 fi
     update-alternatives --install /usr/bin/python python /usr/bin/python2 1
     update-alternatives --install /usr/bin/python python /usr/bin/python3 2
-    #update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90
-    #update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 90
 
